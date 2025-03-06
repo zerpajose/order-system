74bae87b0ecfe92bd5eced019414c68ba3c0daf9
Jose Zerpa <zjose88@gmail.com>
2025-03-06 07:39:05 -0300

    Migrate orders-service from gRPC to HTTP; update Go version to 1.23.5 and refactor order creation logic

--- orders-service/go.mod
+++ orders-service/go.mod
@@ -1,18 +1,3 @@
 module orders-service
 
-go 1.21
-
-require (
-	github.com/zerpajose/order-system/proto v0.0.0
-	google.golang.org/grpc v1.64.0
-	google.golang.org/protobuf v1.36.5
-)
-
-require (
-	golang.org/x/net v0.32.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
-)
-
-replace github.com/zerpajose/order-system/proto => ../proto
+go 1.23.5
--- orders-service/main.go
+++ orders-service/main.go
@@ -1,61 +1,54 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-	"net"
-	"google.golang.org/grpc"
-	pb "github.com/zerpajose/order-system/proto/orders"
-	productpb "github.com/zerpajose/order-system/proto/products"
+	"net/http"
 )
 
-type server struct {
-	pb.UnimplementedOrderServiceServer
-	productClient productpb.ProductServiceClient
+type Product struct {
+	ProductID string  `json:"product_id"`
+	Name      string  `json:"name"`
+	Price     float64 `json:"price"`
+	Stock     int32   `json:"stock"`
 }
 
-func (s *server) CreateOrder(ctx context.Context, req *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
-	// Get product details for each product in the order
-	var products []*productpb.ProductResponse
-	for _, productID := range req.ProductIds {
-		product, err := s.productClient.GetProduct(ctx, &productpb.ProductRequest{ProductId: productID})
-		if err != nil {
-			return nil, err
+type Order struct {
+	OrderID    string    `json:"order_id"`
+	ProductIDs []string  `json:"product_ids"`
+	Products   []Product `json:"products"`
+}
+
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	var order Order
+	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for _, productID := range order.ProductIDs {
+		resp, err := http.Get(fmt.Sprintf("http://products-service:50051/product?product_id=%s", productID))
+		if err != nil || resp.StatusCode != http.StatusOK {
+			http.Error(w, "Error fetching product details", http.StatusInternalServerError)
+			return
 		}
-		products = append(products, product)
+		var product Product
+		if err := json.NewDecoder(resp.Body).Decode(&product); err != nil {
+			http.Error(w, "Error decoding product details", http.StatusInternalServerError)
+			return
+		}
+		order.Products = append(order.Products, product)
 	}
 
 	// TODO: Save order to database
 
-	return &pb.CreateOrderResponse{
-		OrderId:  req.OrderId,
-		Products: products,
-	}, nil
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(order)
 }
 
 func main() {
-	// Set up gRPC connection to products service
-	productConn, err := grpc.Dial("products-service:50051", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("failed to connect to products service: %v", err)
-	}
-	defer productConn.Close()
-
-	// Create the server instance with product client
-	s := &server{
-		productClient: productpb.NewProductServiceClient(productConn),
-	}
-
-	// Start the gRPC server
-	lis, err := net.Listen("tcp", ":50052")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	grpcServer := grpc.NewServer()
-	pb.RegisterOrderServiceServer(grpcServer, s)
-
-	log.Printf("Orders service listening at %v", lis.Addr())
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	http.HandleFunc("/orders", createOrder)
+	log.Println("Orders service listening on port 50052")
+	log.Fatal(http.ListenAndServe(":50052", nil))
 }
